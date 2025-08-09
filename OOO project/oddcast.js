128fb132aa6ce78f878a622d8f6893ea8e737b63
Britney Rooplal <britney.m.rooplal@gmail.com>
2025-08-09 10:31:26 -0400

    Staged changes for OddCast Project (to check if preview works)
    Co-authored-by: LisutSu <LisutSu@users.noreply.github.com>
    Co-authored-by: shawngokul <shawngokul@users.noreply.github.com>
    Co-authored-by: irpl <irpl@users.noreply.github.com>

--- OOO project/oddcast.js
+++ OOO project/oddcast.js
@@ -1,1 +1,38 @@
-var jikanAPI = "https://jikan.moe"
+    var animeList = [1281, 6213, 38000, 53065]
+
+    var jikanAPIURL = "https://api.jikan.moe/v4/anime/";
+
+    var cardContainer = document.getElementById("card-container");
+
+    async function getAnimeData(animeID) {
+      var response = await fetch(jikanAPIURL + animeID);
+
+      if (response.status != 200) {
+        console.log("Error getting data from api")
+        return;
+      }
+
+      var animeData = await response.json();
+
+      var animeTitleEN = animeData.data.title_english;
+      var animeImageURL = animeData.data.images.jpg.image_url
+      console.log(animeTitleEN)
+
+      var cardTemplate = `
+      <div class="card">
+        <img src="${animeImageURL}" alt="Avatar" style="width:100%">
+        <div class="container">
+          <h4><b>${animeTitleEN}</b></h4>
+          <p>${animeTitleEN}</p>
+        </div>
+      </div>
+      `
+      var animeDiv = document.createElement("div");
+      animeDiv.innerHTML = cardTemplate;
+      cardContainer.append(animeDiv);
+
+    }
+
+    for (let i = 0; i < animeList.length; i++) {
+      getAnimeData(animeList[i])
+    }
