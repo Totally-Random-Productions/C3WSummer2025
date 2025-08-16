8e431ceb3f6fe6b6f157d0efd128709aeb2ac100
Britney Rooplal <britney.m.rooplal@gmail.com>
2025-08-16 11:46:22 -0400

    Changes made today, including linking a youtube video, editing some of the css etc

--- OOO project/oddcast.js
+++ OOO project/oddcast.js
@@ -1,11 +1,40 @@
-var animeList = [1281, 6213, 38000, 53065, 1918, 59845, 59062, 60285, 59459, 696, 20];
+var animeList = [ , , , , ,,  ,, ,,
+ ,,, , ,];
+
+  var animeList =[
+
+
+  {id:1281, ytLink:""},
+    {id:6213, ytLink:""},
+      {id:38000, ytLink:""},
+        {id:53065, ytLink:""},
+          {id:1918, ytLink:""},  
+          {id:59845, ytLink:""},
+            {id: 59062, ytLink:""},
+              {id:60285, ytLink:""},
+                {id:59459, ytLink:""},
+                  {id: 696, ytLink:""},
+                    {id:20, ytLink:"https://youtu.be/-G9BqkgZXRA?si=X5q4B5Y8sklP6236"},
+                      {id:2107, ytLink:""},
+                        {id: 22319, ytLink:""},
+                          {id:11061, ytLink:""},
+                            {id:31964, ytLink:""},
+                              {id:32281, ytLink:""},
+                                {id:49596, ytLink:""},
+                                  {id: 47917, ytLink:""},
+
+
+  ];
+
+  var ytLinks = [ ]
 
     var jikanAPIURL = "https://api.jikan.moe/v4/anime/";
 
     var cardContainer = document.getElementById("card-container");
 
-    async function getAnimeData(animeID) {
+    async function getAnimeData(animeID, ytLink) {
       var response = await fetch(jikanAPIURL + animeID);
+      
 
       if (response.status != 200) {
         console.log("Error getting data from api")
@@ -20,7 +49,8 @@ var animeList = [1281, 6213, 38000, 53065, 1918, 59845, 59062, 60285, 59459, 696
     
 
       var cardTemplate = `
-      <div class="card">
+        <a href="${ytLink}">
+        <div class="card">
         <img src="${animeImageURL}" alt="Avatar" style="width:100%">
         <div class="container"
           <h4><b>${animeTitleEN}</b></h4>
@@ -28,6 +58,7 @@ var animeList = [1281, 6213, 38000, 53065, 1918, 59845, 59062, 60285, 59459, 696
           <p></p>
         </div>
       </div>
+      </a> 
       `
       var animeDiv = document.createElement("div");
       animeDiv.innerHTML = cardTemplate;
@@ -36,5 +67,11 @@ var animeList = [1281, 6213, 38000, 53065, 1918, 59845, 59062, 60285, 59459, 696
     }
 
     for (let i = 0; i < animeList.length; i++) {
-      getAnimeData(animeList[i])
+      getAnimeData(animeList[i].id, animeList[i].ytLink);
+
+
+    
+
+
+
     }
